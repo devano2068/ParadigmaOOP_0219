5dac65ceee59da77df95b84fee25247e2c7af948
devano2068 <devano27921@gmail.com>
2025-06-02 20:28:18 +0700

    menjalankan baseclass

--- noOverridinf.cpp
+++ noOverridinf.cpp
@@ -14,13 +14,8 @@ class darivedClass : public baseclass
 {
 public:
     void perkenalan()
-   
+    {
+        cout << "Halo saya function dari derived Class";
+    }
+};
 
-int main()
-{
-    darivedClass a;
-    a.perkenalan();
-
-    return 0;
-
-}
