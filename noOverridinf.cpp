eb0294cde6d3ff0d3e397233fa3deb16eb931944
devano2068 <devano27921@gmail.com>
2025-06-02 20:27:05 +0700

    class darivedClass : public baseclass

--- noOverridinf.cpp
+++ noOverridinf.cpp
@@ -5,12 +5,12 @@ class baseclass
 {
 public:
     virtual void perkenalan() final
-  
+    {
+        cout << "Halo saya function dari base class";
+    }
+};
 
-class darivedClass : public baseclass
-{
-public:
-    void perkenalan()
+
     {
         cout << "Halo saya function dari derived Class";
     }
