5e431fe4558ed5744677fee8c0c611fd8b7fbb49
devano2068 <devano27921@gmail.com>
2025-06-02 20:33:21 +0700

    menjalankan class remotelampu

--- enkapulasi.cpp
+++ enkapulasi.cpp
@@ -1,21 +1,20 @@
 #include <iostream>
 using namespace std;
 
-
-
-int main()
+class remoteLampu
 {
-    remoteLampu lampuRumah;
+private:
+    string saklarNo[10];
 
-    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
-    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
-    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
-    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+public:
+    void setSaklarNo(int i, string value)
+    {
+        saklarNo[i] = value;
+    }
+    string getSaklarNo(int i)
+    {
+        return saklarNo[i];
+    }
+};
 
-    cout << lampuRumah.getSaklarNo(0) << endl;
-    cout << lampuRumah.getSaklarNo(1) << endl;
-    cout << lampuRumah.getSaklarNo(2) << endl;
-    cout << lampuRumah.getSaklarNo(3) << endl;
 
-    return 0;
-}
